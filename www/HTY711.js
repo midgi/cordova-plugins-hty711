941da45877bf435c55b69e8e351103de15f1e506
jose <midgardemon@gmail.com>
2021-08-30 20:16:58 -0400

    se usa arg0 a secas sin los brackets para archivo js

--- www/HTY711.js
+++ www/HTY711.js
@@ -1,37 +1,37 @@
 var exec = require('cordova/exec');
 
 exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'HTY711', 'coolMethod', [arg0]);
+    exec(success, error, 'HTY711', 'coolMethod', arg0);
 };
 
 exports.customInput = function (arg0, success, error) {
-    exec(success, error, 'HTY711', 'customInput', [arg0]);
+    exec(success, error, 'HTY711', 'customInput', arg0);
 };
 
 exports.init = function (arg0, success, error) {
-    exec(success, error, 'HTY711', 'init', [arg0]);
+    exec(success, error, 'HTY711', 'init', arg0);
 };
 
 exports.readGiftCard = function (arg0, success, error) {
-    exec(success, error, 'HTY711', 'readGiftCard', [arg0]);
+    exec(success, error, 'HTY711', 'readGiftCard', arg0);
 };
 
 exports.clearDisplay = function (arg0, success, error) {
-    exec(success, error, 'HTY711', 'clearDisplay', [arg0]);
+    exec(success, error, 'HTY711', 'clearDisplay', arg0);
 };
 
 exports.displayText = function (arg0, success, error) {
-    exec(success, error, 'HTY711', 'displayText', [arg0]);
+    exec(success, error, 'HTY711', 'displayText', arg0);
 };
 
 exports.isConnected = function (arg0, success, error) {
-    exec(success, error, 'HTY711', 'isConnected', [arg0]);
+    exec(success, error, 'HTY711', 'isConnected', arg0);
 };
 
 exports.startScan = function (arg0, success, error) {
-    exec(success, error, 'HTY711', 'startScan', [arg0]);
+    exec(success, error, 'HTY711', 'startScan', arg0);
 };
 
 exports.stopScan = function (arg0, success, error) {
-    exec(success, error, 'HTY711', 'stopScan', [arg0]);
+    exec(success, error, 'HTY711', 'stopScan', arg0);
 };
